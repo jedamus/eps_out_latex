f60768a0ec2ba3c0f8d2155dfaa9d331ad30f349
Leander Jedamus <ljedamus@web.de>
2019-05-10 14:08:32 +0200

    added sed.sh

---
+++ sed.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# erzeugt Donnerstag, 09. Mai 2019 17:43 (C) 2019 von Leander Jedamus
+# modifiziert Freitag, 10. Mai 2019 14:02 von Leander Jedamus
+# modifiziert Donnerstag, 09. Mai 2019 18:19 von Leander Jedamus
+
+sed 's/.*\/\(.*\)/\1/' | sed 's/_/\\_/g'
+
+# vim:ai sw=2
+
